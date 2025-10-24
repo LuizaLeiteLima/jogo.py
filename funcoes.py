118abb1b753c5cb17ecc6adea4cf7e8ed15b5edf
Luiza <luizall3@al.insper.edu.br>
2025-10-24 12:35:29 -0300

    funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -70,8 +70,6 @@ def afundados (frota,tabuleiro):
 def posicao_valida (frota,linha, coluna, orientacao, tamanho):
     x,y = linha,coluna
     pos = []
-    if len(frota) < 0 :
-        return False
     for i in range(tamanho):
         if orientacao == 'horizontal':
             pos.append([x,y+i])
