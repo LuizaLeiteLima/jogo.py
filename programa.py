9cf68da31de1ab70bceed08f2ecc09a113ad8946
Luiza <luizall3@al.insper.edu.br>
2025-10-24 19:50:16 -0300

    programa.py

--- programa.py
+++ programa.py
@@ -1,8 +1,5 @@
 from funcoes import * 
 
-linha = int(input("coloque a linha:"))
-coluna = int(input("coloque a coluna:"))
-orientacao = int(input("coloque a orientação:"))
 
 
 frota = {
@@ -11,27 +8,55 @@ frota = {
     "contratorpedeiro":[],
     "submarino": [],
 }  
-tamanho = 1
-if orientacao == 1:
-    orientacao == 'vertical'
 
-else:
-    orientacao == 'horizontal' 
+navio = {
+    "porta-aviões": 4,
+    "navio-tanque": 3,
+    "contratorpedeiro": 2,
+    "submarino": 1,
+}  
+
 
 i = 0
 
-for nome in frota.keys():
+for nome, tamanho in frota.items():
+    linha = int(input("coloque a linha:"))
+    coluna = int(input("coloque a coluna:"))
+    orientacao = int(input("coloque a orientação:"))
+
+    if nome == "submarino":
+        pass
+    else:
+        if orientacao == 1:
+            orientacao = 'vertical'
+        else:
+            orientacao = 'horizontal'
+
+
+
     print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho} ")
 
-    if posicao_valida == False:
+    # if posicao_valida == False:
+    #     print("Esta posição não está válida!")
+    #     linha = int(input("coloque a linha:"))
+    #     coluna = int(input("coloque a coluna:"))
+    # if posicao_valida == True:
+    #     if define_posicoes(linha, coluna, orientacao, tamanho):
+    #        frota[nome] = preenche_frota(frota,nome, linha, coluna, orientacao, tamanho).append(define_posicoes(linha, coluna, orientacao, tamanho))
+    #        linha = int(input("coloque a linha:"))
+    #        coluna = int(input("coloque a coluna:"))
+    #        i += 1
+    #        tamanho += 1
+
+    if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
         print("Esta posição não está válida!")
         linha = int(input("coloque a linha:"))
         coluna = int(input("coloque a coluna:"))
-    if posicao_valida == True:
+    else:
         if define_posicoes(linha, coluna, orientacao, tamanho):
-           frota[nome] = preenche_frota(frota,nome, linha, coluna, orientacao, tamanho).append(define_posicoes(linha, coluna, orientacao, tamanho))
+           frota = preenche_frota(frota,nome, linha, coluna, orientacao, tamanho)
            linha = int(input("coloque a linha:"))
            coluna = int(input("coloque a coluna:"))
            i += 1
-           tamanho += 1
-    print(frota)
+
+print(frota)
