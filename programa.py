6792212790b1ae5ed1fa04e478ec8a679f6c34d9
Luiza <luizall3@al.insper.edu.br>
2025-10-24 20:51:18 -0300

    programa.py

--- programa.py
+++ programa.py
@@ -22,22 +22,12 @@ for nome in frota.keys():
         quantidade = 4
 
     for v in range(quantidade):
-        print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
-        linha = int(input("coloque a linha:"))
-        coluna = int(input("coloque a coluna:"))
-
-        if nome != "submarino":
-            orientacao = int(input("coloque a orientação:"))
-            if orientacao == 1:
-                orientacao = 'vertical'
-            elif orientacao == 2:
-                orientacao = 'horizontal'
-
-        if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
-            print("Esta posição não está válida!")
+        f = False
+        while not f:
             print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
             linha = int(input("coloque a linha:"))
             coluna = int(input("coloque a coluna:"))
+
             if nome != "submarino":
                 orientacao = int(input("coloque a orientação:"))
                 if orientacao == 1:
@@ -47,9 +37,10 @@ for nome in frota.keys():
 
             if posicao_valida(frota, linha, coluna, orientacao, tamanho) == False:
                 print("Esta posição não está válida!")
-                continue
-
-        define_posicoes(linha, coluna, orientacao, tamanho)
-        frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+            else:
+                define_posicoes(linha, coluna, orientacao, tamanho)
+                frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+                f = True
 
 print(frota)
+
